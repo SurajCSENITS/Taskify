c44fb6107618acea2a8208904a7d872ae8ebad13
SurajCSENITS <138098058+SurajCSENITS@users.noreply.github.com>
2023-12-10 18:34:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import Inputfield from './Components/Inputfield';
 
 const App: React.FC = () => { // arrow function 
   const myRef = useRef<HTMLInputElement>(null);
+  function enableDM(){
+    if(myRef.current){
+      myRef.current.classList.toggle("dark_clr")
+    }
+  }
+
   return (
     <div className="App" ref={myRef}>
+      <div className='dark_mode'><button className='dm_btn' onClick={enableDM}><img src="https://cdn-icons-png.flaticon.com/512/6714/6714978.png" alt="DM" /></button></div>
       <span className='heading'>Taskify</span>
       <Inputfield myRef={myRef}/>
     </div>
